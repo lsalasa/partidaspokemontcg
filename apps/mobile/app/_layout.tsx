3b9a2e5ca9d303ef59de85fa61df1904d93c12b1
howlerdev506 <ppedroza558@gmail.com>
2024-12-07 19:03:02 -0600

    clean files
    remove un used files

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,6 +1,4 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
@@ -45,7 +43,6 @@ const styles = StyleSheet.create({
   },
   gameContainer: {
     flex: 1,
-    alignItems: 'center',
     paddingVertical: 20,
   }
 });
